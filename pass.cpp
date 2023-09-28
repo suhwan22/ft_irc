5ad4265af0ba5744bfeac1f2038598ca1353d108
jaeywon <dnsjwodyd@gmail.com>
2023-09-28 23:14:05 +0900

    커서오류 해결

--- pass.cpp
+++ pass.cpp
@@ -3,6 +3,5 @@
 
 void	cmd::pass(string pass)
 {
-	pass.erase(pass.size() -1, 1);
 	searchClient(_clntSock)->setPass(pass);
 }
--- util.cpp
+++ util.cpp
@@ -7,7 +7,7 @@ void cmd::noSuchNick(string wrongnick) {
 	string	msg;
 	Client	*me = searchClient(_clntSock);
 
-	msg = ":irc.local 401 " + me->getNickname() + wrongnick + ":No such nick\n";
+	msg = ":irc.local 401 " + me->getNickname() + " " + wrongnick + " :No such nick\n";
 	if (send(_clntSock, msg.c_str(), msg.length(), 0) == -1)
 		cerr << "Error: send err" << endl;
 }
@@ -16,7 +16,8 @@ void cmd::noSuchChannel(string wrongchannel) {
 	string	msg;
 	Client	*me = searchClient(_clntSock);
 
-	msg = ":irc.local 403 " + me->getNickname() + wrongchannel + ":No such channel\n";
+	
+	msg = ":irc.local 403 " + me->getNickname() + " " + wrongchannel + " :No such channel\n";
 	if (send(_clntSock, msg.c_str(), msg.length(), 0) == -1)
 		cerr << "Error: send err" << endl;
 }
