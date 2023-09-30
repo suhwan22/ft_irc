263c2b36b91d9320c6d3a40b8b7251665e56af99
jaeywon <dnsjwodyd@gmail.com>
2023-09-30 17:58:09 +0900

    add mode.cpp-1

--- mode.cpp
+++ mode.cpp
@@ -10,15 +10,29 @@ void cmd::modeToClient(string arg, string line)
 	cout << "MODE CLIENT = " << arg << "MODE option = " << line << "MODE MSG = " << line_two << endl;
 }
 
+void cmd::modeToClient(string arg, string line)
+{
+	string line_two;
+	stringstream tmp;
+	tmp = stringstream(line);
+	tmp >> line;
+	getline(tmp, line_two, static_cast<char>(EOF));
+	cout << "MODE CLIENT = " << arg << "MODE option = " << line << "MODE MSG = " << line_two << endl;
+}
+
 void cmd::mode(string arg)
 {
+	string msg;
 	string line;
 	stringstream tmp;
 	tmp = stringstream(arg);
 	tmp >> arg;
 	getline(tmp, line, static_cast<char>(EOF));
 	if (arg[0] == '#')
-		modeToClient(arg, line);
-		// modeToChannel(arg, line);
-	// else
+		modeToChannel(arg, line);
+	else {
+		msg = "Error: parameters error\n";
+		if (send(_clntSock, msg.c_str(), msg.size(), 0) == -1)
+			cerr << "Error: send error" << endl;
+	}
 }
