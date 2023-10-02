b5122bc8cc152d175efa9e29682c3b1fa9ae0155
원서윤 <wonseoyun@wonseoyun-ui-MacBookPro.local>
2023-10-02 22:04:01 +0900

    msg ficished

--- privmsg.cpp
+++ privmsg.cpp
@@ -24,7 +24,7 @@ void cmd::privmsgToChannel(string arg, string inputmsg) {
 				for (int i = 0; i < (int)members.size(); i++)
 				{
 					msg = ":" + me->getNickname() + "!" + me->getUserName() + "@" + me->getIP() \
-						   + " PRIVMSG " + arg + " " + inputmsg;
+						   + " PRIVMSG " + arg + " " + inputmsg + "\r\n";
 					if (members[i]->getSock() != _clntSock && send(members[i]->getSock(), msg.c_str(), msg.size(), 0) == -1)
 						cerr << "Error: send error" << endl;
 				}
