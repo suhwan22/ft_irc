143cbd74786163836862e6699aef6933b68c2e01
suhwan <tnghkswkd123@naver.com>
2023-10-02 23:23:52 +0900

    quit server에 들어와 있는 client한테 다 보내야해서 수정함

--- quit.cpp
+++ quit.cpp
@@ -3,11 +3,15 @@
 
 void	cmd::quit(string arg)
 {
-	Client	*client = searchClient(_clntSock);
+	Client	*me = searchClient(_clntSock);
 	string	msg;
 
-	msg = "ERROR :Closing link: (" + client->getUserName() + "@127.0.0.1) [Quit: " + arg + "]\r\n";
+	msg = "ERROR :Closing link: (" + me->getUserName() + "@127.0.0.1) [Quit: " + arg + "]\r\n";
 
 	if (send(_clntSock, msg.c_str(), msg.size(), 0) == -1)
 		cout << "quit send fail?" << endl;
+	msg = ":" + me->getNickname() + "!" + me->getUserName() + "@127.0.0.1 QUIT :Quit: " + arg + "\r\n";
+	for (vector<Client *>::iterator it = _clntList.begin(); it != _clntList.end(); it++)
+		if ((*it) != me)
+			send((*it)->getSock(), msg.c_str(), msg.size(), 0);
 }
