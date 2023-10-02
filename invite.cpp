56a247a93547156b9230afda47ef06d67d6a5eb2
suhwan <tnghkswkd123@naver.com>
2023-10-03 07:45:08 +0900

    빈 채널 지워지는 거는 됐고 invite channel part 되는지 확인해 보자

--- invite.cpp
+++ invite.cpp
@@ -43,25 +43,34 @@ void	cmd::invite(string arg)
 		}
 	else
 	{
-		ch->addInviteUser(me);
-		channel_users = ch->getUsersName();
-		users = ch->getUsers();
-
-		/* 초대를 한 client에게 보내는 메세지 */
-		msg = ":irc.local 341 " + me->getNickname() + " " \
-			   + client->getNickname() + "  :" + ch->getChannelName() + "\r\n";
-		send(_clntSock, msg.c_str(), msg.size(), 0);
-
-		/* 초대를 받은 client에게 보내는 메세지 */
-		msg = ":irc.local NOTICE " + ch->getChannelName() \
-				+ " :*** " + me->getNickname() + " invited test into the channel\r\n";
-		send(client->getSock(), msg.c_str(), msg.size(), 0);
-
-		/* 초대한 채널에 있는 client들에게 보내는 메세지 */
-		msg = ":" + me->getNickname() + "!" + me->getUserName() \
-			   + "@127.0.0.1 INVITE " + client->getNickname() + " :" + ch->getChannelName() + "\r\n";
-		for (vector<Client *>::iterator it = users.begin(); it != users.end(); it++)
-			if ((*it) != me)
-				send((*it)->getSock(), msg.c_str(), msg.size(), 0);
+		if (ch->isClientInvite(client))
+		{
+			msg = ":irc.local 443 " + me->getNickname() + " " \
+				   + client->getNickname() + " " + ch->getChannelName() + " :is already on channel\r\n";
+			send(_clntSock, msg.c_str(), msg.size(), 0);
+		}
+		else
+		{
+			ch->addInviteUser(me);
+			channel_users = ch->getUsersName();
+			users = ch->getUsers();
+	
+			/* 초대를 한 client에게 보내는 메세지 */
+			msg = ":irc.local 341 " + me->getNickname() + " " \
+				   + client->getNickname() + "  :" + ch->getChannelName() + "\r\n";
+			send(_clntSock, msg.c_str(), msg.size(), 0);
+	
+			/* 초대를 받은 client에게 보내는 메세지 */
+			msg = ":irc.local NOTICE " + ch->getChannelName() \
+					+ " :*** " + me->getNickname() + " invited test into the channel\r\n";
+			send(client->getSock(), msg.c_str(), msg.size(), 0);
+	
+			/* 초대한 채널에 있는 client들에게 보내는 메세지 */
+			msg = ":" + me->getNickname() + "!" + me->getUserName() \
+				   + "@127.0.0.1 INVITE " + client->getNickname() + " :" + ch->getChannelName() + "\r\n";
+			for (vector<Client *>::iterator it = users.begin(); it != users.end(); it++)
+				if ((*it) != me)
+					send((*it)->getSock(), msg.c_str(), msg.size(), 0);
+		}
 	}
 }
