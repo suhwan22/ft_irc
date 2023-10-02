e73afe7ce67c840565927037fdfd4bb3eb83ff51
suhwan <tnghkswkd123@naver.com>
2023-10-02 12:44:00 +0000

    modified for lobby exit error

--- server.cpp
+++ server.cpp
@@ -87,23 +87,20 @@ void	Server::serverStart()
 			else
 			{
 				strlen = recv(epEvents[i].data.fd, buf, BUF_SIZE, 0);
-				if (strlen == -1)
-				{
-					std::cout << "Error: serverStart(): recv()" << std::endl;
-				}
-
-				cmd command(epEvents[i].data.fd, buf, strlen, _passWord, _clntList, _channelList);
-				command.printContent(command.getContent());
-
-				if (strlen == 0)
+				if (strlen <= 0)
 				{
 					epoll_ctl(epfd, EPOLL_CTL_DEL, epEvents[i].data.fd, NULL);
 					std::cout << "closed client: " << epEvents[i].data.fd << std::endl;
-					//shutdown(epEvents[i].data.fd, SHUT_RDWR);
 					close(epEvents[i].data.fd);
 				}
 				else
+				{
+					cmd command(epEvents[i].data.fd, buf, strlen, _passWord, _clntList, _channelList);
+					command.printContent(command.getContent());
 					command.parsecommand();
+				}
+
+
 			}
 		}
 	}
