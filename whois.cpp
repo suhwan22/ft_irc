3b092df71f2f5404e75380b0cdd6fa8689c866ec
원서윤 <wonseoyun@wonseoyun-ui-MacBookPro.local>
2023-10-03 13:46:45 +0900

    add whois

--- whois.cpp
+++ whois.cpp
@@ -4,15 +4,15 @@
 
 void	cmd::whois(string nick)
 {
-	string	msg;
+	string	msg, msg1, msg2, msg3;
 	Client	*me = searchClient(_clntSock);
 	// Client	*who = searchClient(nick);
 
-	msg = ":irc.local 311 " + me->getNickname() + " " + nick + " " + me->getUserName() +  " " + me->getIP() + " * :" + me->getUserName() + "\r\n" + \
-	":irc.local 319 " + me->getNickname() + " " + nick + " :#123\r\n" + \ 
-	":irc.local 312 " + me->getNickname() + " " + nick + " irc.local :Local IRC Server\r\n" + \
-	":irc.local 318 " + me->getNickname() + " " + nick + " :End of /WHOIS list.\r\n";
-	send(_clntSock, msg.c_str(), msg.size(), 0);
+	msg = ":irc.local 311 " + me->getNickname() + " " + nick + " " + me->getUserName() +  " " + me->getIP() + " * :" + me->getUserName() + "\r\n";
+	msg1 = ":irc.local 319 " + me->getNickname() + " " + nick + " :#123\r\n"; 
+	msg2 = ":irc.local 312 " + me->getNickname() + " " + nick + " irc.local :Local IRC Server\r\n";
+	msg3 = ":irc.local 318 " + me->getNickname() + " " + nick + " :End of /WHOIS list.\r\n";
+	send(_clntSock, (msg + msg1 + msg2 + msg3).c_str(), (msg + msg1 + msg2 + msg3).size(), 0);
 }
 
 // :irc.local 311 test2 test root 127.0.0.1 * :root
@@ -20,4 +20,5 @@ void	cmd::whois(string nick)
 // :irc.local 312 test2 test irc.local :Local IRC Server
 // :irc.local 317 test2 test 873 1696305780 :seconds idle, signon time
 // :irc.local 318 test2 test :End of /WHOIS list.
+
 
